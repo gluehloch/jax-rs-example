9f24b0d1d72e23c2948bb406f358e409c0e5a897
Andre Winkler <gluehloch@googlemail.com>
2022-03-13 13:12:05 +0100

    It is startable from Eclipse.

--- readme.md
+++ readme.md
@@ -3,3 +3,9 @@
 ## Common Jersey Use Cases
 
 https://eclipse-ee4j.github.io/jersey.github.io/documentation/latest3x/modules-and-dependencies.html#d0e560
+
+## How to use
+
+```
+curl http://localhost:9998/notifications/helloworld
+```
--- src/main/java/de/awtools/rest/Main.java
+++ src/main/java/de/awtools/rest/Main.java
@@ -2,20 +2,27 @@ package de.awtools.rest;
 
 import java.net.URI;
 
-import com.sun.net.httpserver.HttpServer;
-import jakarta.ws.rs.core.UriBuilder;
+import javax.swing.JOptionPane;
+
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.sun.net.httpserver.HttpServer;
+
+import jakarta.ws.rs.core.UriBuilder;
+
 public class Main {
-    
+
     public static void main(String[] args) {
         URI baseUri = UriBuilder.fromUri("http://localhost/").port(9998).build();
         ResourceConfig config = new ResourceConfig(RestEndpoint.class);
         // RestApplication restApplication = new RestApplication();
 
         HttpServer httpServer = JdkHttpServerFactory.createHttpServer(baseUri, config);
-        httpServer.start();
+        // httpServer.start();
+
+        JOptionPane.showMessageDialog(null, "Ende");
+        httpServer.stop(0);
     }
 
 }
